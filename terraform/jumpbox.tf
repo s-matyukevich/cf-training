30bec9c88871b812c295a9e12ee75aeac68ef7d5
s-matyukevich <s.matyukevich@gmail.com>
2017-06-08 16:41:17 -0600

    disk size cahnged

--- terraform/jumpbox.tf
+++ terraform/jumpbox.tf
@@ -138,6 +138,11 @@ resource "aws_instance" "training_jumpbox" {
     associate_public_ip_address = true
     source_dest_check = false
 
+    root_block_device {
+        volume_size =  "100"
+        volume_type = "gp2"
+    }
+
     tags {
         Name = "jumpbox_${var.name_tag}"
         Owner = "${var.owner_tag}"
